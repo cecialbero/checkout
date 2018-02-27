b7f62c3a3c0b23e9786254cf2f034f150416ff02
Cecilia Albero <calbero@itx.com>
2018-02-27 10:15:53 -0300

    Set the range logic to print the user and amount value

--- checkout-page/src/app/slider/slider.component.ts
+++ checkout-page/src/app/slider/slider.component.ts
@@ -9,6 +9,7 @@ export class SliderComponent implements OnInit {
 
   monthValue = true;
   yearValue = false;
+  businessAmountPerYear = 600;
 
   startup = {
     users: 5,
@@ -38,6 +39,31 @@ export class SliderComponent implements OnInit {
   onSelectYear() {
     this.monthValue = false;
     this.yearValue = true;
+  }
+
+  valueRange(value) {
+    this.businessAmountPerYear = value;
+
+    switch (value) {
+      case '600':
+        this.business.users = 5;
+        break;
+
+      case '700':
+        this.business.users = 6;
+        break;
+
+      case '800':
+        this.business.users = 7;
+        break;
+
+      case '900':
+        this.business.users = 8;
+        break;
+
+      default:
+        this.business.users = 10;
+    }
   }
 
 }
