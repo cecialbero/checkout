90640a89208a90f65bd538e287c23fb9122de04a
Cecilia Albero <calbero@itx.com>
2020-03-01 22:05:47 -0300

    Sets the router link to the contact us page and replace the mural email

---
+++ checkout-page/src/app/contact-us/contact-us.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.scss']
+})
+export class ContactUsComponent implements OnInit {
+
+  name: string;
+  phone: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  inputsName(event: any) {
+    this.name = event.target.value;
+  }
+
+  inputsPhone(event: any) {
+    this.phone = event.target.value;
+  }
+
+}
