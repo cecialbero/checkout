65dd4de5695451da9272697c630f207fba791aea
Cecilia Albero <calbero@itx.com>
2020-04-28 20:31:47 -0300

    Creates the title property for the user-type

--- checkout-page/src/app/home/user-type/user-type.component.ts
+++ checkout-page/src/app/home/user-type/user-type.component.ts
@@ -11,6 +11,7 @@ export class UserTypeComponent {
   public startupPlan: any;
   public businessPlan: any;
   public enterprisePlan: any;
+  public title = 'CHOOSE THIS PLAN IF YOU\RE A...';
 
   constructor(private service: UserTypeService) {
     this.startupPlan = service.getStartupPlan();
