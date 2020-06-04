f9653823eb2b2656106fe7d311dde9a84d5f2cc8
Cecilia Albero <calbero@itx.com>
2020-06-04 10:05:29 -0300

    Imports the form module and reactive forms modules

--- checkout-page/src/app/app.module.ts
+++ checkout-page/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
 // Components
 import { HeaderModule } from './shared/components/header/header.module';
 
@@ -10,12 +12,12 @@ import { HeaderModule } from './shared/components/header/header.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HeaderModule
   ],
