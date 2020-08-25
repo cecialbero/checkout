ee00ad51ae039731dc9fdffd3cfc0fc63a0a9548
Cecilia Albero <calbero@itx.com>
2020-08-25 20:53:08 -0300

    Creates the unit test cases for the home component

--- checkout-page/src/app/home/home.component.spec.ts
+++ checkout-page/src/app/home/home.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import {RouterTestingModule} from "@angular/router/testing";
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +10,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [ HttpClientModule, RouterTestingModule ]
     })
     .compileComponents();
   }));
@@ -22,4 +25,19 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain the choose-plan component', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-choose-plan')).not.toBe(null);
+  }));
+
+  it('should contain the user-type component', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-user-type')).not.toBe(null);
+  }));
+
+  it('should contain the current-plan component', async(() => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('app-current-plan')).not.toBe(null);
+  }));
 });
