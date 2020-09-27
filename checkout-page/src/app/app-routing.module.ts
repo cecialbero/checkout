8b8d0f50c379216b95639bc9e08db8654e7ac0de
Cecilia Albero <calbero@itx.com>
2020-09-27 18:22:53 -0300

    Creates the app routing and app component unit testing

--- checkout-page/src/app/app-routing.module.ts
+++ checkout-page/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule) },
   { path: 'contact-us', loadChildren: () => import('./contact-us/contact-us.module').then(mod => mod.ContactUsModule) },
-  { path: 'choose-this-plan', loadChildren: () => import('./choose-this-plan/choose-this-plan.module').then(mod => mod.ChooseThisPlanModule) },
+  { path: 'choose-this-plan', loadChildren: () => import('./choose-this-plan/choose-this-plan.module').then(mod => mod.ChooseThisPlanModule) }
 ];
 
 @NgModule({
